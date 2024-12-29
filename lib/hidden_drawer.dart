323c56e69b1ca12d827ec5db84a4fc9d2a446b13
Gee <gladysmbuthia324@gmail.com>
2024-12-29 15:48:15 +0300

    separated _pages to a diff file

--- lib/hidden_drawer.dart
+++ lib/hidden_drawer.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_practice/pages/drawer_pages/account_page.dart';
-import 'package:firebase_practice/pages/home_screen.dart';
-import 'package:firebase_practice/utils/theme_provider.dart';
+import 'package:firebase_practice/hidden_drawer_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
-import 'package:provider/provider.dart';
 
 class HiddenDrawer extends StatefulWidget {
   const HiddenDrawer({super.key});
@@ -21,33 +18,8 @@ class _HiddenDrawerState extends State<HiddenDrawer> {
 
     // Initialize pages in didChangeDependencies to ensure Theme.of(context) works
 
-    _pages = [
-      ScreenHiddenDrawer(
-        ItemHiddenMenu(
-          name: 'HomeScreen',
-          baseStyle: const TextStyle(),
-          selectedStyle: const TextStyle(),
-          colorLineSelected: Theme.of(context).colorScheme.surface,
-          
-        ),
-        Theme(
-          data: Provider.of<ThemeProvider>(context).themeData,
-          child: const HomeScreen(),
-        ),
-      ),
-      ScreenHiddenDrawer(
-        ItemHiddenMenu(
-          name: 'AccountPage',
-          baseStyle: const TextStyle(),
-          selectedStyle: const TextStyle(),
-          colorLineSelected: Theme.of(context).colorScheme.secondary,
-        ),
-        Theme(
-          data: Provider.of<ThemeProvider>(context).themeData,
-          child: const AccountPage(),
-        ),
-      ),
-    ];
+    // Initialize pages in didChangeDependencies to ensure Theme.of(context) works
+    _pages = getDrawerPages(context);  // Retrieve the pages from the new file
   }
 
   @override
@@ -61,6 +33,7 @@ class _HiddenDrawerState extends State<HiddenDrawer> {
       isDraggable: true,
       slidePercent: 50,
       contentCornerRadius: 20,
+      
     );
   }
 }
---
+++ lib/hidden_drawer_pages.dart
@@ -0,0 +1,115 @@
+// hidden_drawer_pages.dart
+
+import 'package:firebase_practice/pages/drawer_pages/account_page.dart';
+import 'package:firebase_practice/pages/drawer_pages/devices_page.dart';
+import 'package:firebase_practice/pages/drawer_pages/help_page.dart';
+import 'package:firebase_practice/pages/drawer_pages/manage_users_page.dart';
+import 'package:firebase_practice/pages/drawer_pages/music_page.dart';
+import 'package:firebase_practice/pages/drawer_pages/rooms_page.dart';
+import 'package:firebase_practice/pages/drawer_pages/settings_page.dart';
+import 'package:firebase_practice/pages/home_screen.dart';
+import 'package:firebase_practice/utils/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
+import 'package:provider/provider.dart';
+
+List<ScreenHiddenDrawer> getDrawerPages(BuildContext context) {
+  return [
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Home',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.surface,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const HomeScreen(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'My Account',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const AccountPage(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Manage Users',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const ManageUsersPage(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Devices',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const DevicesPage(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Rooms',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const RoomsPage(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Music',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const MusicPage(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Settings',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const SettingsPage(),
+      ),
+    ),
+    ScreenHiddenDrawer(
+      ItemHiddenMenu(
+        name: 'Help',
+        baseStyle: const TextStyle(),
+        selectedStyle: const TextStyle(),
+        colorLineSelected: Theme.of(context).colorScheme.secondary,
+      ),
+      Theme(
+        data: Provider.of<ThemeProvider>(context).themeData,
+        child: const HelpPage(),
+      ),
+    ),
+  ];
+}
